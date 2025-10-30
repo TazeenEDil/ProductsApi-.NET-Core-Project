3f874c53c4bcaa2177124656529cd9e0a55049c2
TazeenEDil <tazeenedil470@gmail.com>
2025-10-31 01:33:31 +0500

    Add project files.

---
+++ ASPCoreWebAPI/Controllers/ErrorController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductsApi.Controllers
+{
+    [ApiController]
+    [ApiExplorerSettings(IgnoreApi = true)] 
+    public class ErrorController : ControllerBase
+    {
+        [Route("/error")]
+        [HttpGet] 
+        public IActionResult HandleError()
+        {
+            var exception = HttpContext.Features.Get<IExceptionHandlerFeature>();
+
+            return Problem(
+                detail: exception?.Error.Message,
+                title: "An unexpected error occurred!"
+            );
+        }
+    }
+}
---
+++ ASPCoreWebAPI/Program.cs
@@ -0,0 +1,23 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers()
+       .AddNewtonsoftJson();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Centralized exception handling built-in
+app.UseExceptionHandler("/error");
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
